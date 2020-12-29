483c4ea1341ecff87ae546d09a8d3b337827b6dd
arc6828 <chavalit.kow@gmail.com>
2020-12-29 21:35:21 +0700

    API

---
+++ app/Http/Controllers/API/Export_2hs_timeController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Export_2hs_time;
+
+class Export_2hs_timeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $year_at = $request->get('year_at');
+        $exporter = $request->get('exporter');
+        $importer = $request->get('importer');
+        $hscode = $request->get('hscode');
+        $value = $request->get('value');
+        $unit = $request->get('unit');
+        $trade_type = $request->get('trade_type');
+
+        if (!empty($year_at)) {
+            $export_2hs_time = Export_2hs_time::select('id','year_at','exporter','hscode','value')
+                ->where('year_at', "LIKE" ,"%$year_at%")
+                ->where('exporter', 'LIKE', "%$exporter%")
+                // ->where('importer', 'LIKE', "%$importer%")
+                ->where('hscode', 'LIKE', "%$hscode%")
+                // ->where('value', 'LIKE', "%$value%")
+                // ->where('unit', 'LIKE', "%$unit%")
+                // ->where('trade_type', 'LIKE', "%$trade_type%")
+                ->get();
+        } else {
+            // $export_2hs_time = Export_2hs_time::latest()->paginate($perPage);
+            $export_2hs_time = Export_2hs_time::select('id','year_at','exporter','hscode','value')
+                ->limit(1000)
+                ->get();
+        }
+        return $export_2hs_time;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 
 Route::get('product','API\ProductController@index');
+
+Route::get('export-2hs','API\Export_2hs_timeController@index');
